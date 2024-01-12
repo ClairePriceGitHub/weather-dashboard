30872032873335824ea017f4faf450d39bc5949b
Claire Price <claire.price@mail.com>
2024-01-12 22:14:18 +0000

    add forecast-values.js

---
+++ assets/forecast-values.js
@@ -0,0 +1,38 @@
+var forecastValues = [
+    {
+        day: 0,
+        temp: 0,
+        wind: 0,
+        humidity: 0
+    },
+    {
+        day: 1,
+        temp: 0,
+        wind: 0,
+        humidity: 0
+    },
+    {
+        day: 2,
+        temp: 0,
+        wind: 0,
+        humidity: 0
+    },
+    {
+        day: 3,
+        temp: 0,
+        wind: 0,
+        humidity: 0
+    },
+    {
+        day: 4,
+        temp: 0,
+        wind: 0,
+        humidity: 0
+    },
+    {
+        day: 5,
+        temp: 0,
+        wind: 0,
+        humidity: 0
+    }
+]
