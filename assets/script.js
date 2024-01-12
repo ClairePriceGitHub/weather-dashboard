85bb31f9524184aef2cbbda2d5ef31777836e757
Claire Price <claire.price@mail.com>
2024-01-12 18:55:23 +0000

    add styling to appended buttons

--- assets/script.js
+++ assets/script.js
@@ -2,15 +2,21 @@ const queryApi = 'a26f42af7a79688b36f7437ceea8de52';
 
 let cities = ['Berlin', 'Paris', 'Edinburgh', 'Madrid', 'Birmingham', 'London'];
 
-
 $('.search-button').on('click', (event) => {
     event.preventDefault();
     const addButton = $('<button>');
+    addButton.css({
+        'width': '100%',
+        'height': '5vh',
+        'backgroundColor': 'lightGrey',
+        'borderRadius' : '5px',
+        'border' : 'none',
+        'margin-bottom' : '15px'
+    });
     const weatherSearch = $('.weather-search');
     let input = weatherSearch.val();  
     addButton.text(input);
     $('.input-group-append').append(addButton);
-    console.log(input);
 })
 
 // for (var i=0; i < cities.length; i++) {
