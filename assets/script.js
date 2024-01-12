04c054942f6ce2718f6dd9b49e729bb5f5dfb364
Claire Price <claire.price@mail.com>
2024-01-12 14:04:13 +0000

    add city buttons

--- assets/script.js
+++ assets/script.js
@@ -1,1 +1,10 @@
 const queryApi = 'a26f42af7a79688b36f7437ceea8de52';
+
+let cities = ['Berlin', 'Paris', 'Edinburgh', 'Madrid', 'Birmingham', 'London'];
+
+for (var i=0; i < cities.length; i++) {
+    const addButton = $('<button>');
+    addButton.text(cities[i]);
+   // addButton.attr('cityname', cities[i])
+    $('.input-group-append').append(addButton);
+}
