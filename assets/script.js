8a33ba174f0a24b75690a0cc0c09fa0d32dd7667
Claire Price <claire.price@mail.com>
2024-01-12 18:34:46 +0000

    on click creates button

--- assets/script.js
+++ assets/script.js
@@ -2,9 +2,20 @@ const queryApi = 'a26f42af7a79688b36f7437ceea8de52';
 
 let cities = ['Berlin', 'Paris', 'Edinburgh', 'Madrid', 'Birmingham', 'London'];
 
-for (var i=0; i < cities.length; i++) {
+
+$('.search-button').on('click', (event) => {
+    event.preventDefault();
     const addButton = $('<button>');
-    addButton.text(cities[i]);
-   // addButton.attr('cityname', cities[i])
+    const weatherSearch = $('.weather-search');
+    let input = weatherSearch.val();  
+    addButton.text(input);
     $('.input-group-append').append(addButton);
-}
+    console.log(input);
+})
+
+// for (var i=0; i < cities.length; i++) {
+//     const addButton = $('<button>');
+//     addButton.text(cities[i]);
+//    // addButton.attr('cityname', cities[i])
+//     $('.input-group-append').append(addButton);
+// }
